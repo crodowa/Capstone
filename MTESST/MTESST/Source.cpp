ea761cabb58dba9a0c42e26f0db360cf2168ae05
crodowa <crodowa@uwo.ca>
2018-01-22 22:53:56 -0500

    Threading and button pressing added

--- MTESST/MTESST/Source.cpp
+++ MTESST/MTESST/Source.cpp
@@ -7,114 +7,284 @@
 
 #include "icon.xpm"
 
+
+/*
 class MySIN : public mpFX
 {
-	double m_freq, m_amp;
+double m_freq, m_amp;
 public:
-	MySIN(double freq, double amp) : mpFX(wxT("f(x) = SIN(x)"), mpALIGN_LEFT) { m_freq = freq; m_amp = amp; m_drawOutsideMargins = false; }
-	virtual double GetY(double x) { return m_amp * sin(2 * M_PI*m_freq*x); }
-	virtual double GetMinY() { return -m_amp; }
-	virtual double GetMaxY() { return  m_amp; }
+MySIN(double freq, double amp) : mpFX(wxT("f(x) = SIN(x)"), mpALIGN_LEFT) { m_freq = freq; m_amp = amp; m_drawOutsideMargins = false; }
+virtual double GetY(double x) { return m_amp * sin(2 * M_PI*m_freq*x); }
+virtual double GetMinY() { return -m_amp; }
+virtual double GetMaxY() { return  m_amp; }
 };
+*/
+
+
+wxDECLARE_EVENT(myEVT_THREAD_UPDATE, wxThreadEvent);
+
+class MyFrame : public wxFrame, public wxThreadHelper
+{
+public:
+	MyFrame();
+	~MyFrame();
+	void OnThreadUpdate(wxThreadEvent& evt);
+	void OnClose(wxCloseEvent& evt);
+	void StartAlongTask();
+	void Show();
+	void OnSettingButtonClicked(wxCommandEvent&);
+	void OnRunButtonClicked(wxCommandEvent&);
+	void OnCancelButtonClicked(wxCommandEvent&);
+protected:
+	virtual wxThread::ExitCode Entry();
+	wxDECLARE_EVENT_TABLE();
+
+private:
+	wxFrame * window;
+	wxGrid* m_data;
+	mpWindow* m_plot;
+	//char data = 'c';
+	std::vector<double> vectorx, vectory;
+	mpFXYVector* vectorLayer;
+	wxTextCtrl *displacement;
+	wxTextCtrl *frequency;
+	wxTextCtrl *cycles;
+	wxTextCtrl *force;
+	wxTextCtrl *diameter;
+	bool cancel = false;
+};
+
+//(indentor_app);
+wxDEFINE_EVENT(myEVT_THREAD_UPDATE, wxThreadEvent);
+wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
+EVT_THREAD(myEVT_THREAD_UPDATE, MyFrame::OnThreadUpdate)
+EVT_CLOSE(MyFrame::OnClose)
+wxEND_EVENT_TABLE()
+
+MyFrame::MyFrame()
+{
+	window = new wxFrame(NULL, -1, "MTESST", wxDefaultPosition, wxSize(700, 750));
+	window->Fit();
+	wxIcon *icon = new wxIcon(_e0968bce85d4723c058a9ee8c33f35c);
+	window->SetIcon(*icon);
+
+	//End of window creation
+
+	//Menu Creation
+	wxMenuBar* menuBar = new wxMenuBar();
+	wxMenu *fileMenu = new wxMenu();
+	fileMenu->Append(wxID_OPEN, _T("&Open"));
+	fileMenu->Append(wxID_SAVE, _T("&Save"));
+	fileMenu->AppendSeparator();
+	fileMenu->Append(wxID_EXIT, _T("&Quit"));
+	menuBar->Append(fileMenu, _T("&File"));
+	wxMenu *helpMenu = new wxMenu();
+	helpMenu->Append(wxID_ABOUT, _T("&About"));
+	menuBar->Append(helpMenu, _T("&Help"));
+	window->SetMenuBar(menuBar);
+	//End of Menu
+
+
+	wxBoxSizer *topsizer = new wxBoxSizer(wxVERTICAL);
+
+	//Settings
+	wxBoxSizer *settings = new wxBoxSizer(wxHORIZONTAL);
+
+	displacement = new wxTextCtrl(window, -1);
+	settings->Add(new wxStaticText(window, -1, "Enter Desired Displacement(mm):"), 0, wxALL, 12);
+	settings->Add(displacement, 0, wxALL, 10);
+
+	frequency = new wxTextCtrl(window, -1);
+	settings->Add(new wxStaticText(window, -1, "Enter Desired Frequency(Hz):"), 0, wxALL, 12);
+	settings->Add(frequency, 0, wxALL, 10);
+
+	cycles = new wxTextCtrl(window, -1);
+	settings->Add(new wxStaticText(window, -1, "Enter Desired Number of Cycles:"), 0, wxALL, 12);
+	settings->Add(cycles, 0, wxALL, 10);
+
+	wxBoxSizer *settings2 = new wxBoxSizer(wxHORIZONTAL);
+
+	force = new wxTextCtrl(window, -1);
+	settings2->Add(new wxStaticText(window, -1, "Enter Max Allowable Force(N):      "), 0, wxALL, 12);
+	settings2->Add(force, 0, wxALL, 10);
+
+	diameter = new wxTextCtrl(window, -1);
+	settings2->Add(new wxStaticText(window, -1, "Enter Tip Diameter(mm):       "), 0, wxALL, 12);
+	settings2->Add(diameter, 0, wxALL, 10);
+
+	wxButton *settingsbut = new wxButton(window, -1, "Enter Settings");
+	settings2->Add(settingsbut, 0, wxALL, 10);
+	settingsbut->Bind(wxEVT_BUTTON, &MyFrame::OnSettingButtonClicked, this);
+	//End of settings
+
+	//Start of Graph and text panel
+	wxNotebook *notebook = new wxNotebook(window, wxID_ANY);
+	wxBoxSizer *data = new wxBoxSizer(wxHORIZONTAL);
+
+	wxFont graphFont(11, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+	m_plot = new mpWindow(notebook, -1, wxDefaultPosition, wxSize(500, 300), wxSUNKEN_BORDER);
+	mpScaleX* xaxis = new mpScaleX(wxT("X"), mpALIGN_BOTTOM, true, mpX_NORMAL);
+	mpScaleY* yaxis = new mpScaleY(wxT("Y"), mpALIGN_LEFT, true);
+	vectorLayer = new mpFXYVector(_("Vector"));
+	xaxis->SetFont(graphFont);
+	yaxis->SetFont(graphFont);
+	xaxis->SetDrawOutsideMargins(false);
+	yaxis->SetDrawOutsideMargins(false);
+	m_plot->SetMargins(50, 50, 50, 100);
+	m_plot->AddLayer(xaxis);
+	m_plot->AddLayer(yaxis);
+	//m_plot->AddLayer(new MySIN(1.0, 1.0));
+	m_plot->AddLayer(vectorLayer);
+
+	vectorLayer->SetData(vectorx, vectory);
+	vectorLayer->SetContinuity(true);
+	wxPen vectorpen(*wxBLUE, 2, wxSOLID);
+	vectorLayer->SetPen(vectorpen);
+	vectorLayer->SetDrawOutsideMargins(false);
+
+	m_data = new wxGrid(notebook, -1, wxDefaultPosition, wxSize(500, 300));
+	m_data->CreateGrid(0, 0);
+	m_data->AppendRows(10);
+	m_data->AppendCols(3);
+	m_data->EnableEditing(false);
+	m_data->SetColLabelValue(0, "Time(s)");
+	m_data->SetColLabelValue(1, "Force(mN)");
+	m_data->SetColLabelValue(2, "Displacement(mm)");
+	m_data->Fit();
+
+	notebook->AddPage(m_plot, wxT("Gragh"));
+	notebook->AddPage(m_data, wxT("Text"));
+	data->Add(notebook, 1, wxGROW | wxALL, 10);
+	//End of Graph and text panel
+
+	//Start of Control Buttons (not include setting button)
+
+	static const wxString label[] = { "Graph","Data" };
+	wxBoxSizer *control = new wxBoxSizer(wxVERTICAL);
+	//control->Add(new wxRadioBox(window, -1, "Data", wxDefaultPosition, wxDefaultSize, 2, label, 1, wxRA_SPECIFY_COLS), 0, wxALL, 10);
+	wxButton *cancel = new wxButton(window, -1, "Cancel");
+	wxButton *run = new wxButton(window, -1, "Run");
+	control->Add(new wxButton(window, -1, "Jog Indentor"), 0, wxALL, 10);
+	control->Add(cancel, 0, wxALL, 10);
+	cancel->Bind(wxEVT_BUTTON, &MyFrame::OnCancelButtonClicked, this);
+	control->Add(run, 0, wxALL, 10);
+	run->Bind(wxEVT_BUTTON, &MyFrame::OnRunButtonClicked, this);
+
+	//End of Control Buttons (not include setting button)
+
+	data->Add(control, 0, wxALIGN_BOTTOM);
+	topsizer->Add(settings, 0);
+	topsizer->Add(settings2, 0);
+	topsizer->Add(data, 1, wxEXPAND);
+
+	window->SetSizerAndFit(topsizer);
+}
+
+MyFrame::~MyFrame()
+{
+
+}
+
+wxThread::ExitCode MyFrame::Entry()
+{
+	wxThreadEvent evt(wxEVT_THREAD, myEVT_THREAD_UPDATE);
+	int p = -100;
+	while (1) {
+		if (cancel == true)
+		{
+			break;
+		}
+		else if (!GetThread()->TestDestroy())
+		{
+			p++;
+			vectorx.push_back(((double)p - 50.0)*5.0);
+			vectory.push_back(0.0001*pow(((double)p - 50.0)*5.0, 3));
+			//m_data->SetCellValue(0, 0, data);
+			//data = 'd';
+			//wxThread::Sleep(500);
+			//vectorx.push_back(p);
+			//vectory.push_back(sin(p));
+			//this->Show();
+			//wxQueueEvent(this, evt.Clone());
+		}
+		//break;
+		wxThread::Sleep(10);
+		wxQueueEvent(this, evt.Clone());
+
+	}
+	return (wxThread::ExitCode)0;
+}
+
+void MyFrame::OnThreadUpdate(wxThreadEvent& evt)
+{
+	vectorLayer->SetData(vectorx, vectory);
+	m_plot->Fit();
+	//m_plot->AddLayer(vectorLayer);
+}
+
+void MyFrame::OnClose(wxCloseEvent&)
+{
+	if (GetThread() && GetThread()->IsRunning())
+		GetThread()->Wait();
+	Destroy();
+}
+
+void MyFrame::StartAlongTask()
+{
+	if (CreateThread(wxTHREAD_JOINABLE) != wxTHREAD_NO_ERROR)
+	{
+		wxLogError("Could not create the worker thread!");
+		return;
+	}
+	if (GetThread()->Run() != wxTHREAD_NO_ERROR)
+	{
+		wxLogError("Could not run the worker thread!");
+		return;
+	}
+}
+
+void MyFrame::Show()
+{
+	window->Show();
+}
+
+void MyFrame::OnSettingButtonClicked(wxCommandEvent&)
+{
+	wxString settingValues = displacement->GetValue();
+	settingValues.Append("mm displacement \n");
+	settingValues.Append(frequency->GetValue());
+	settingValues.Append("Hz frequency \n");
+	settingValues.Append(cycles->GetValue());
+	settingValues.Append(" cycles \n");
+	settingValues.Append(force->GetValue());
+	settingValues.Append("N maximum force\n");
+	settingValues.Append(diameter->GetValue());
+	settingValues.Append("mm diameter tip");
+
+	wxMessageBox(settingValues, "Settings Entered", wxOK | wxCANCEL);
+}
+
+void MyFrame::OnCancelButtonClicked(wxCommandEvent&)
+{
+	cancel = true;
+}
+
+void MyFrame::OnRunButtonClicked(wxCommandEvent&)
+{
+	this->StartAlongTask();
+	cancel = false;
+	vectorx.clear();
+	vectory.clear();
+}
 
 class indentor_app : public wxApp
 {
 public:
 	bool OnInit()
 	{
-		//Create main window
-		wxFrame* window = new wxFrame(NULL, -1, "MTESST", wxDefaultPosition, wxSize(700, 750));
-		window->Fit();
-		wxIcon *icon = new wxIcon(_e0968bce85d4723c058a9ee8c33f35c);
-		window->SetIcon(*icon);
-
-		//End of window creation
-
-		//Menu Creation
-		wxMenuBar* menuBar = new wxMenuBar();
-		wxMenu *fileMenu = new wxMenu();
-		fileMenu->Append(wxID_OPEN, _T("&Open"));
-		fileMenu->Append(wxID_SAVE, _T("&Save"));
-		fileMenu->AppendSeparator();
-		fileMenu->Append(wxID_EXIT, _T("&Quit"));
-		menuBar->Append(fileMenu, _T("&File"));
-		wxMenu *helpMenu = new wxMenu();
-		helpMenu->Append(wxID_ABOUT, _T("&About"));
-		menuBar->Append(helpMenu, _T("&Help"));
-		window->SetMenuBar(menuBar);
-		//End of Menu
-
-
-		wxBoxSizer *topsizer = new wxBoxSizer(wxVERTICAL);
-
-		//Settings
-		wxBoxSizer *settings = new wxBoxSizer(wxHORIZONTAL);
-		settings->Add(new wxStaticText(window, -1, "Enter Desired Displacement(mm):"), 0, wxALL, 12);
-		settings->Add(new wxTextCtrl(window, -1), 0, wxALL, 10);
-		settings->Add(new wxStaticText(window, -1, "Enter Desired Frequency(Hz):"), 0, wxALL, 12);
-		settings->Add(new wxTextCtrl(window, -1), 0, wxALL, 10);
-		settings->Add(new wxStaticText(window, -1, "Enter Desired Number of Cycles:"), 0, wxALL, 12);
-		settings->Add(new wxTextCtrl(window, -1), 0, wxALL, 10);
-
-		wxBoxSizer *settings2 = new wxBoxSizer(wxHORIZONTAL);
-		settings2->Add(new wxStaticText(window, -1, "Enter Max Allowable Force(N):      "), 0, wxALL, 12);
-		settings2->Add(new wxTextCtrl(window, -1), 0, wxALL, 10);
-		settings2->Add(new wxStaticText(window, -1, "Enter Tip Diameter(mm):       "), 0, wxALL, 12);
-		settings2->Add(new wxTextCtrl(window, -1), 0, wxALL, 10);
-		settings2->Add(new wxButton(window, -1, "Enter Settings"), 0, wxALL, 10);
-		//End of settings
-
-		//Start of Graph and text panel
-		wxNotebook *notebook = new wxNotebook(window, wxID_ANY);
-		wxBoxSizer *data = new wxBoxSizer(wxHORIZONTAL);
-		
-		wxFont graphFont(11, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
-		mpWindow* m_plot = new mpWindow(notebook, -1, wxDefaultPosition, wxSize(500, 300), wxSUNKEN_BORDER);
-		mpScaleX* xaxis = new mpScaleX(wxT("X"), mpALIGN_BOTTOM, true, mpX_NORMAL);
-		mpScaleY* yaxis = new mpScaleY(wxT("Y"), mpALIGN_LEFT, true);
-		xaxis->SetFont(graphFont);
-		yaxis->SetFont(graphFont);
-		xaxis->SetDrawOutsideMargins(false);
-		yaxis->SetDrawOutsideMargins(false);
-		m_plot->SetMargins(50, 50, 50, 100);
-		m_plot->AddLayer(xaxis);
-		m_plot->AddLayer(yaxis);
-		m_plot->AddLayer(new MySIN(1.0, 1.0));
-
-		wxGrid* m_data = new wxGrid(notebook, -1, wxDefaultPosition, wxSize(500, 300));
-		m_data->CreateGrid(0, 0);
-		m_data->AppendRows(10);
-		m_data->AppendCols(3);
-		m_data->EnableEditing(false);
-		m_data->SetColLabelValue(0,"Time(s)");
-		m_data->SetColLabelValue(1, "Force(mN)");
-		m_data->SetColLabelValue(2, "Displacement(mm)");
-		m_data->Fit();
-		
-		notebook->AddPage(m_plot, wxT("Gragh"));
-		notebook->AddPage(m_data, wxT("Text"));
-		data->Add(notebook, 1, wxGROW | wxALL, 10);
-		//End of Graph and text panel
-
-		//Start of Control Buttons (not include setting button)
-
-		static const wxString label[] = { "Graph","Data" };
-		wxBoxSizer *control = new wxBoxSizer(wxVERTICAL);
-		//control->Add(new wxRadioBox(window, -1, "Data", wxDefaultPosition, wxDefaultSize, 2, label, 1, wxRA_SPECIFY_COLS), 0, wxALL, 10);
-		control->Add(new wxButton(window, -1, "Jog Indentor"), 0, wxALL, 10);
-		control->Add(new wxButton(window, -1, "Cancel"), 0, wxALL, 10);
-		control->Add(new wxButton(window, -1, "Run"), 0, wxALL, 10);
-
-		//End of Control Buttons (not include setting button)
-
-		data->Add(control, 0, wxALIGN_BOTTOM);
-		topsizer->Add(settings, 0);
-		topsizer->Add(settings2, 0);
-		topsizer->Add(data, 1, wxEXPAND);
-
-		window->SetSizerAndFit(topsizer);
-
-		window->Show();
+		MyFrame* frame = new MyFrame();
+		//frame->StartAlongTask();
+		frame->Show();
 		return true;
 	}
 };
